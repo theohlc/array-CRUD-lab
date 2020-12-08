528e600a023cfcdcf3383587aaff8ede33bcf31c
theo <teddyhoover@gmail.com>
2020-12-08 16:14:45 -0600

    commit tree 53e8beab8804ef2add10be2ac08e032a363d31c4

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+    my_array = []
 end
 
 def create_an_array
-  
+    my_array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    #my_array = array.push(element)
+    my_array = array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    my_array = array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    my_array = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    my_array = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    result = array [index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array.last
 end
